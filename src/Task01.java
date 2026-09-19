061c934e720483231993077b6c7cd913bc7e1521
nahomair716 <nahomair716@gmail.com>
2026-09-19 11:58:21 -0400

    Update Task01.java

--- src/Task01.java
+++ src/Task01.java
@@ -1,2 +1,10 @@
 public class Task01 {
+    public void main() {
+double itemPrice = 102.99;
+double itemTax = itemPrice * 0.05;
+double total = itemPrice + itemTax;
+        System.out.println(" Your item costs $" + itemPrice + " and has a tax of $" + itemTax+". so your toal is $" + total);
+
+
+    }
 }
